23e4520150168bb26f425ea327ff6fa15f5f4626
Wonders Victor <wondersprince1@gmail.com>
2025-01-31 23:26:03 +0100

    Fix GitHub URL in response object

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.get('/', (req: Request, res: Response) => {
   const response: InfoResponse = {
     email: "wondersprince1@gmail.com", // Replace with your email
     current_datetime: new Date().toISOString(),
-    github_url: "https://https://github.com/Wondahs/hng-stage0-api.git" // Replace with your repo URL
+    github_url: "https://github.com/Wondahs/hng-stage0-api" // Replace with your repo URL
   };
   
   res.status(200).json(response);
